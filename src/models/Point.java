9a1a83b5b71fddc4871bd5d31c14eb3b9bfdcc4a
yerassylv <bolatkannn@gmail.com>
2023-12-24 20:56:23 +0600

    corrected

--- src/models/Point.java
+++ src/models/Point.java
@@ -18,9 +18,6 @@ public class Point{
 
 
     public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        return "Point{" + "x=" + x + ", y=" + y + '}';
     }
 }
