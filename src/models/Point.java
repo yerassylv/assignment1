f138ba1314941a5b22017bb1174a1dcd0eb86435
yerassylv <bolatkannn@gmail.com>
2023-12-24 18:04:48 +0600

    f

--- src/models/Point.java
+++ src/models/Point.java
@@ -1,4 +1,27 @@
 package models;
 
-public class Point {
+public class Point{
+    private final double x;
+
+    private final double y;
+
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getDistanceTo(Point dest) {
+        return Math.sqrt(
+                Math.pow(this.x - dest.x, 2) + Math.pow(this.y - dest.y, 2)
+        );
+    }
+
+
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,4 +1,29 @@
 package models;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Shape {
+    List<Point> points = new ArrayList<>();
+    public void addPoint(Point newPoint) {
+        points.add(newPoint);
+    }
+    public double getPerimeter() {
+        int count = points.size();
+        double perimeter = 0;
+        for (int i = 0; i < count; i++)
+            perimeter += points.get(i).getDistanceTo(points.get((i + 1) % count));
+        return perimeter; }
+    public double getAverage() {
+        int count = points.size();
+        if (count <= 1) return 0;
+        if (count == 2) return getPerimeter();
+        return getPerimeter() / count;
+    }
+    public double getLongest() {
+        double mx = 0;
+        int size = points.size();
+        for (int i = 0; i < size; i++)
+            mx = Math.max(points.get(i).getDistanceTo(points.get((i + 1) % size)), mx);
+        return mx;
+    }
+
 }
