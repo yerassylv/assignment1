c08a6ecbad11efca092de7d80c88eadca25f04b2
yerassylv <bolatkannn@gmail.com>
2023-12-24 18:10:23 +0600

    added classes (point,shape )

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -3,7 +3,7 @@ import models.Shape;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.Scanner:
 
 public class MyApplication {
     public static void main(String[] args) throws FileNotFoundException {
